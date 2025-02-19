52723eb840f01cd47d0ea69c14c77fb54aef7808
Peter Bollhorn <pbollhorn@gmail.com>
2025-02-19 13:15:03 +0100

    started work on getAdultMoviesByReleaseYear

--- src/main/java/app/services/MovieService.java
+++ src/main/java/app/services/MovieService.java
@@ -80,6 +80,29 @@ public class MovieService {
         return null;
     }
 
+    public static List<MovieDto> getAdultMoviesByReleaseYear(int year) {
+
+        String key = System.getenv("api_key");
+        String url = "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&primary_release_year=" + year + "&sort_by=popularity.desc&api_key=" + key;
+
+        String json = ApiReader.getDataFromUrl(url);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+
+        try {
+            HelperDto helperDto = objectMapper.readValue(json, HelperDto.class);
+
+            return helperDto.results;
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 
     private static class HelperDto {
 
