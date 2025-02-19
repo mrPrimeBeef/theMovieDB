8699dbda31b73481c6ff69b213fc864d2560c6e0
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-02-19 13:03:07 +0100

    f

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,7 @@ public class Main {
 //        MovieDto movieDto = MovieService.getMovieDtoById("tt0120685");
 //        System.out.println(movieDto);
 
-        MovieDto[] list = MovieService.getMoviesByRating(7.5,8);
+        MovieDto[] list = MovieService.getMoviesByRating(9,10);
 
         Arrays.stream(list).forEach(System.out::println);
 
