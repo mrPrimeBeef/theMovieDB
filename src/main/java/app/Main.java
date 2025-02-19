ffe74b71b29ac1a638bd4144d396571dc6c1aabb
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-02-19 09:49:34 +0100

    updates

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,45 @@
 package app;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String key = System.getenv("api_key");
+
+        String response = getDataFromURL("https://api.themoviedb.org/3/find/tt0903747?external_source=imdb_id&api_key=" + key);
+
+        System.out.println(response);
+    }
+
+    public static String getDataFromURL(String url){
+
+        String body = null;
+        try {
+            // Create an HttpClient instance
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create a request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .uri(new URI(url))
+                    .GET()
+                    .build();
+
+            // Send the request and get the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Check the status code and print the response
+            if (response.statusCode() >= 200 && response.statusCode() < 300 ) {
+                body = response.body();
+            } else {
+                System.out.println("GET request failed. Status code: " + response.statusCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return body;
     }
 }
