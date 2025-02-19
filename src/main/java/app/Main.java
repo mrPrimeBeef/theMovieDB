d206725e34f48325e70c8416c99802cb41c5f55e
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-02-19 10:24:35 +0100

    DTO

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String key = System.getenv("api_key");
 
-        String response = ApiReader.getDataFromURL("https://api.themoviedb.org/3/find/tt0903747?external_source=imdb_id&api_key=" + key);
+        String response = ApiReader.getDataFromURL("https://api.themoviedb.org/3/search/movie?query=godzilla&include_adult=false&language=en-US&api_key=" + key);
 
         System.out.println(response);
     }
